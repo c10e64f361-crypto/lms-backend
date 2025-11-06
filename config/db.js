c39b0095ca97f3fb625eb3c737dcce991e8003ca
c10e64f361-crypto <c10e64f361@gmail.com>
2025-11-06 15:15:58 +0700

    db

--- config/db.js
+++ config/db.js
@@ -1,17 +1,38 @@
+// // config/db.js
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DB
+// });
+
+// connection.connect(err => {
+//   if (err) console.error('MySQL connection error:', err);
+//   else console.log('MySQL Connected!');
+// });
+
+// module.exports = connection;
+//raiwway
 // config/db.js
 const mysql = require('mysql2');
-require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
-connection.connect(err => {
-  if (err) console.error('MySQL connection error:', err);
-  else console.log('MySQL Connected!');
+db.connect(err => {
+  if (err) {
+    console.error('MySQL Connection Error:', err);
+    process.exit(1);
+  }
+  console.log('MySQL Connected to Railway!');
 });
 
-module.exports = connection;
+module.exports = db;
